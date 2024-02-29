57695ae241d1144a771051a6ba087dc99d124c0c
MGRichardson <142840558+MGRichardson@users.noreply.github.com>
2024-02-29 14:30:45 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,27 @@
 #main
 
-def character_counter(str):
+def character_counter(string):
     #accepts a string as a argument
     #has lists of consonants and vowels
     #compare str to lists
     #each time a character is found in a list
     #it adds to either consonant or vowel depending on list found in
     
+    #base lists and varibles
+    vowels = "aeiou"
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    con_found = 0
+    vow_found = 0
     
+    #find amount of vowels
+    for character in string:
+        if character in vowels:
+            vow_found += 1
+    
+    #find amount of consonants
+    for character in string:
+        if character in consonants:
+            con_found += 1
+    
+    #return amount each occoured
+    return con_found, vow_found
