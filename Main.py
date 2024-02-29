6f8f440961731563f147102dc007c9d92f49eccd
MGRichardson <142840558+MGRichardson@users.noreply.github.com>
2024-02-29 14:21:13 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
 #main
+
+def character_counter(str):
+    #accepts a string as a argument
+    #has lists of consonants and vowels
+    #compare str to lists
+    #each time a character is found in a list
+    #it adds to either consonant or vowel depending on list found in
+    
+    
