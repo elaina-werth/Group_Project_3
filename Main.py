d7f93e4ccd95223116de7658cf2e833873042137
elaina-werth <werthemw@gmail.com>
2024-02-29 14:22:08 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,30 @@
 #main
+
+def string_manipulator():
+    #string_manipulator accepts no arguments
+    #it takes a user-inputted string and performs various string manipulations using loops, conditional statements, and functions
+    
+    #keep_going variable
+    keep_going = 'y'
+    
+    #call menu, call chosen function, ask for keepgoing
+    while keep_going == 'y':
+        choice = menu()
+        if choice == 1:
+            input_gathering()
+        elif choice == 2:
+            string_reversal()
+        elif choice == 3:
+            count_vowels()
+        elif choice == 4:
+            character_counter()
+        elif choice == 5:
+            character_replacer
+        elif choice == 6:
+            string_analysis()
+        keep_going = input("Would you like to choose another option? (y/n) ")
+        
+    
+    
+    
+    
