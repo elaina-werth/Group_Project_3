646a606550345e3a4fd75f40bd6ba6546f976874
elaina-werth <werthemw@gmail.com>
2024-03-01 14:50:20 -0600

    Create string_reversal.py

---
+++ string_reversal.py
@@ -0,0 +1,12 @@
+#string_reversal
+
+def string_reversal(string):
+    #string_reversal accepts a string as an argument
+    #it returns its reversed form
+    
+    character_list = []
+    
+    for character in string:
+        character_list.append[character]
+        
+    print (character_list)
